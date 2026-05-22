2091f467506bc7fbb42e494fb2c0257c51a138f2
Omur Emre Yildiz <omuremreyildiz@gmail.com>
2026-05-22 15:33:37 +0300

    Add files via upload

--- hppTEST.hpp
+++ hppTEST.hpp
@@ -1,22 +1,48 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 class Vehicle {
 public:
     std::string brand;
     int year;
 
-    Vehicle(std::string brand, int year);
-    std::string getBrand();
-    int getYear();
-    void honk();
+    Vehicle(std::string brand, int year) {
+        // Initialize vehicle
+        this->brand = brand;
+        this->year = year;
+    }
+
+    std::string getBrand() {
+        // Return brand name
+        return this->brand;
+    }
+
+    int getYear() {
+        return this->year;
+    }
+
+    void honk() {
+        // Make honk sound
+        std::cout << "Beep beep!" << std::endl;
+    }
 };
 
 class Car : public Vehicle {
 public:
     int numDoors;
 
-    Car(std::string brand, int year, int numDoors);
-    void drive();
-    void park();
+    Car(std::string brand, int year, int numDoors) : Vehicle(brand, year) {
+        // Initialize car with door count
+        this->numDoors = numDoors;
+    }
+
+    void drive() {
+        // Start driving
+        std::cout << brand << " is driving." << std::endl;
+    }
+
+    void park() {
+        std::cout << brand << " is parked." << std::endl;
+    }
 };
